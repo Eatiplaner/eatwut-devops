91cc7ab9361d9a4374f5a6d3e9c86dc0cf7afcd2
Louis <huynguyennbk@gmail.com>
2022-07-02 11:54:15 +0700

    feat: update script sync proto to support outdir

--- scripts/proto/sync_proto.sh
+++ scripts/proto/sync_proto.sh
@@ -10,10 +10,14 @@ proto_yaml_file=protodep.yaml
 source=$(yq .source $proto_yaml_file)
 branch=$(yq .branch $proto_yaml_file)
 outpb=$(yq .outpb $proto_yaml_file)
+outdir=$(yq .outdir $proto_yaml_file)
 token_key=$(printenv $(yq .token_key $proto_yaml_file))
 dependencies=$(yq .dependencies $proto_yaml_file)
 
 command=$(yq .command $proto_yaml_file)
+
+rm -rf $outpb/*
+rm -rf $outdir/*
 
 fetch_proto() {
   dependency=$1
@@ -34,10 +38,14 @@ while IFS= read -r value; do
   name=$(parse_name $dependency)
 
   formatted_pb=$(echo $outpb | sed 's/\//\\\//g')
+  formatted_outdir=$(echo $outdir | sed 's/\//\\\//g')
 
   fetch_proto $dependency
 
-  formatted_command=$(echo "$command" | sed "s/\$outpb/$formatted_pb/" | sed "s/\$dependency/$name/")
+  formatted_command=$(echo "$command" | sed "s/\$outpb/$formatted_pb/g" \
+    | sed "s/\$dependency/$name/g" \
+    | sed "s/\$outdir/$formatted_outdir/g" \
+  )
 
   $($formatted_command)
 done < <(yq eval '.dependencies' $proto_yaml_file)
